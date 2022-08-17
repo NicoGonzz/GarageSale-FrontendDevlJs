fd405c02c046297fcd472c8fd6e148c00f4d3f57
NicoGonzz <juannicolas28@live.com>
2022-08-16 23:36:30 -0500

    Agregamos la funcionalidad del carrito de compras para mobile ,desktop y que no se interponga con los otros menus ya creados

--- main.js
+++ main.js
@@ -2,13 +2,33 @@ const menuEmail =document.querySelector('.navbar-email');
 const desktopMenu=document.querySelector('.desktop-menu');
 const menuBurguerIcono=document.querySelector('.menu');
 const mobileMenu=document.querySelector('.mobile-menu');
+const menuCarIcon=document.querySelector('.navbar-shopping-cart');
+const aside=document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuBurguerIcono.addEventListener('click',togglemobileMenu);
+menuCarIcon.addEventListener('click',toggleCarritoAside);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
 }
+
 function togglemobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+
+        if(!isAsideClosed){
+            aside.classList.add('inactive');
+        }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+    const ismobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+       if(!ismobileMenuClosed)
+       {  
+         mobileMenu.classList.add('inactive');//si el menu esta abierto se cierra
+       }     
+            aside.classList.toggle('inactive');
 }
