581654eda205ff440c0bdc28b8d21fd96496e958
NicoGonzz <juannicolas28@live.com>
2022-08-16 22:54:41 -0500

    Menu Mobile con los mediaquery y funcionalidad boton mobile

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const menuEmail =document.querySelector('.navbar-email');
-const desktopMenu=document.querySelector('.desktop-menu')
+const desktopMenu=document.querySelector('.desktop-menu');
+const menuBurguerIcono=document.querySelector('.menu');
+const mobileMenu=document.querySelector('.mobile-menu');
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuBurguerIcono.addEventListener('click',togglemobileMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+function togglemobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
