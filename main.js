0519aff8fb5a3b2ba53c34e37d743bac8e791445
NicoGonzz <juannicolas28@live.com>
2022-08-21 23:18:12 -0500

    Detalles de un producto en html y css

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu=document.querySelector('.desktop-menu');
 const menuBurguerIcono=document.querySelector('.menu');
 const mobileMenu=document.querySelector('.mobile-menu');
 const menuCarIcon=document.querySelector('.navbar-shopping-cart');
-const aside=document.querySelector('.product-detail');
+const shoppingCartContainer=document.querySelector('#shoppingCartContainer');
 const cardsContainer=document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -15,10 +15,10 @@ function toggleDesktopMenu(){
 }
 
 function togglemobileMenu(){
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
         if(!isAsideClosed){
-            aside.classList.add('inactive');
+            shoppingCartContainer.classList.add('inactive');
         }
 
     mobileMenu.classList.toggle('inactive');
@@ -31,7 +31,7 @@ function toggleCarritoAside(){
        {  
          mobileMenu.classList.add('inactive');//si el menu esta abierto se cierra
        }     
-            aside.classList.toggle('inactive');
+       shoppingCartContainer.classList.toggle('inactive');
 }
 
 const productList =[];
